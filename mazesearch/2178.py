b821880276c70ca4cf1e9e8f3516c2b4a8a36e5d
kks <kkisung2657@gmail.com>
2024-03-04 08:38:03 +0900

    baekjoon

--- mazesearch/2178.py
+++ mazesearch/2178.py
@@ -1,25 +1,32 @@
 import sys
+from collections import deque
 sys.stdin = open("input.txt", "r")
 
-def dfs(row, col, cnt):
-    global result
-    if result <= cnt:
-        return
+def bfs(row, col):
+    cnt = 1
+    q = deque([])
+    q.append([row,col,cnt])
+    visited_matrix[row][col] = 1
+    while q:
 
-    if row == N-1 and col == M-1:
-        if result > cnt:
-            result = cnt
-        return
-    for drow, dcol in division:
-        nrow = row + drow
-        ncol = col + dcol
-        if 0<= nrow < N and 0<= ncol < M:
-            if matrix[nrow][ncol] == 1 and visited_matrix[nrow][ncol] == 0:
-                cnt +=1
-                visited_matrix[row][col] = 1
-                dfs(nrow, ncol, cnt)
-                cnt -=1
-                visited_matrix[row][col] = 0
+        vrow, vcol,vcnt = q.popleft()
+        if vrow == N-1 and vcol == M-1 :
+            result.append(vcnt)
+
+        for drow,dcol in division:
+            nrow = vrow + drow
+            ncol = vcol + dcol
+            ncnt = vcnt + 1
+            if 0 <= nrow < N and 0 <= ncol < M and visited_matrix[nrow][ncol] == 0\
+            and matrix[nrow][ncol] == 1:
+                
+                
+                q.append([nrow,ncol,ncnt])
+                visited_matrix[nrow][ncol] = 1
+
+    
+
+
 
 N, M = map(int,input().split())
 matrix = [list(map(int, input())) for _ in range(N)]
@@ -27,6 +34,6 @@ matrix = [list(map(int, input())) for _ in range(N)]
 division = [[1,0],[-1,0],[0,1], [0,-1]]
 visited_matrix = [[0]* M for _ in range(N)]
 # print(visited_matrix)
-result = 1000000
-dfs(0,0,1)
-print(result)
+result =[]
+bfs(0,0)
+print(*result)
